23745f5658395a60449637314a95dd41ff2d36d4
Sergey <ravienserj@gmail.com>
2018-05-03 18:41:42 +0300

    Implement forEach

--- src/arrayMethods.js
+++ src/arrayMethods.js
@@ -1,5 +1,9 @@
-window.every = function () {};
-window.some = function () {};
-window.forEach = function () {};
+window.every = function (array, callback) {};
+window.some = function (array,callback) {};
+window.forEach = function (array,callback) {
+	for(var i = 0; i < array.length; i++){
+		callback(array[i], i, array);
+	}
+};
 window.filter = function () {};
 window.map = function () {};
