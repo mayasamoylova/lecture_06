bd98bc24f7b820fd8ed764c7ac464e7489cb8b39
mayasamoylova <mayasamoylova1@gmail.com>
2018-05-06 23:55:13 +0300

    all tests passed

--- src/arrayMethods.js
+++ src/arrayMethods.js
@@ -1,9 +1,39 @@
-window.every = function (array, callback) {};
-window.some = function (array,callback) {};
-window.forEach = function (array,callback) {
-	for(var i = 0; i < array.length; i++){
-		callback(array[i], i, array);
+window.every = function (arr, callback) {
+	for (var i=0; i<arr.length; i++) {
+		if(!callback(arr[i], i, arr)) {
+			return false;
+		};
+	};
+		return true;
+};
+window.some = function (arr, callback) {
+	for (var i=0; i<arr.length; i++) {
+		if(callback(arr[i], i, arr)) {
+			return true;
+		};
+	};
+		return false;
+};
+window.forEach = function (arr, callback) {
+	for(var i = 0; i < arr.length; i++){
+		callback(arr[i], i, arr);
 	}
 };
-window.filter = function () {};
-window.map = function () {};
+window.filter = function (arr, callback) {
+	var newArr=[];
+	for(var i = 0; i < arr.length; i++){
+		if (callback(arr[i], i, arr)) {
+			newArr.push(arr[i])
+	};
+	};
+	return newArr;
+};
+window.map = function (arr, callback) {
+	var newArr=[];
+	for(var i = 0; i < arr.length; i++){
+	newArr.push(callback(arr[i], i, arr))
+	};
+
+	return newArr;
+	
+};
