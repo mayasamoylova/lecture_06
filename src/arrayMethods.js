6eb59117b554a2b1585033dd6ae5d9f2339ef7ca
mayasamoylova <mayasamoylova1@gmail.com>
2018-05-07 22:15:20 +0300

    attempt 2

--- src/arrayMethods.js
+++ src/arrayMethods.js
@@ -1,39 +1,38 @@
 window.every = function (arr, callback) {
-	for (var i=0; i<arr.length; i++) {
-		if(!callback(arr[i], i, arr)) {
-			return false;
-		};
-	};
-		return true;
+  for (var i = 0; i < arr.length; i++) {
+    if (!callback(arr[i], i, arr)) {
+      return false;
+	}
+  }
+  return true;
 };
 window.some = function (arr, callback) {
-	for (var i=0; i<arr.length; i++) {
-		if(callback(arr[i], i, arr)) {
-			return true;
-		};
-	};
-		return false;
+  for (var i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      return true;
+	}
+  }
+  return false;
 };
 window.forEach = function (arr, callback) {
-	for(var i = 0; i < arr.length; i++){
-		callback(arr[i], i, arr);
-	}
+  for (var i = 0; i < arr.length; i++) {
+    callback(arr[i], i, arr);
+  }
 };
 window.filter = function (arr, callback) {
-	var newArr=[];
-	for(var i = 0; i < arr.length; i++){
-		if (callback(arr[i], i, arr)) {
-			newArr.push(arr[i])
-	};
-	};
-	return newArr;
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
 window.map = function (arr, callback) {
-	var newArr=[];
+  var newArr = [];
 	for(var i = 0; i < arr.length; i++){
-	newArr.push(callback(arr[i], i, arr))
-	};
+	  newArr.push(callback(arr[i], i, arr));
+  }
 
-	return newArr;
-	
+  return newArr;	
 };
